97d3812ba2193c17341978f08d76d5fc2662fbd8
Tnhan <dathilpd@gmail.com>
2022-06-07 00:37:22 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>React App</h1>
-            </header>
-        </div>
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <Routes>
+                        {publicRoutes.map((route, index) => {
+                            const Page = route.component;
+                            return <Route key={index} path={route.path} element={<Page />} />;
+                        })}
+                    </Routes>
+                </header>
+            </div>
+        </Router>
     );
 }
 
